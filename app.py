b22872f323f740e80d7b63f7e846e0a0d4253a81
MrBeam <Beamsangmin@gmail.com>
2022-08-04 16:21:56 +0700

    back

--- app.py
+++ app.py
@@ -18,8 +18,129 @@ def index():
 
 @app.route("/test", methods=['POST'])
 def callback():
+    print('xxxxxx')
     body = request.get_data(as_text=True)
-    print(body)
+    # print(body)
+    req = request.get_json(silent=True, force=True)
+    intent = req["queryResult"]["intent"]["displayName"] 
+    text = req['originalDetectIntentRequest']['payload']['data']['message']['text'] 
+    reply_token = req['originalDetectIntentRequest']['payload']['data']['replyToken']
+    id = req['originalDetectIntentRequest']['payload']['data']['source']['userId']
+    disname = line_bot_api.get_profile(id).display_name
+
+    reply(intent,text,reply_token,id,disname)
+
+    print('id = ' + id)
+    print('name = ' + disname)
+    print('text = ' + text)
+    print('intent = ' + intent)
+    print('reply_token = ' + reply_token)
+    print('\n')
+    
+    if (len(text)):
+        
+        if intent == 'ReplyKIN':
+            replyKIN(intent,text,reply_token,id,disname)
+            quit()
+        if intent == 'ReplySDN':
+            replySDN(intent,text,reply_token,id,disname)
+            quit()
+        if intent == 'Vendor':
+            reply(intent,text,reply_token,id,disname)
+            quit()
+        if intent == 'Customer':
+            reply(intent,text,reply_token,id,disname)
+            quit()
+        if intent == 'Vendor - Description':
+            replyVendor(intent,text,reply_token,id,disname)
+            quit()
+        if intent == 'Customer - Description':
+            replyCustomer(intent,text,reply_token,id,disname)
+            quit()
+        if intent == 'requestCompanyName':
+            reqCompanyName(intent,text,reply_token,id,disname)
+            quit()
+        if intent == 'ReplyKIN':
+            replyKIN(intent,text,reply_token,id,disname)
+            quit()
+        if intent == 'ReplySDN':
+            replySDN(intent,text,reply_token,id,disname)
+            quit()
+        
     return 'OK'
 
+def reply(intent,text,reply_token,id,disname):
+    if intent == 'Vendor':
+        text_message = TextSendMessage(text='Vendor List')
+        line_bot_api.reply_message(reply_token,text_message)
+        
+    if intent == 'Customer':
+        text_message = TextSendMessage(text='Customer List')
+        line_bot_api.reply_message(reply_token,text_message)
+        
+def replyVendor(intent,text,reply_token,id,disname):
+    if intent == 'Vendor - Description':
+        text_message = TextSendMessage(text='Vendor Description')
+        line_bot_api.reply_message(reply_token,text_message)
+
+def replyCustomer(intent,text,reply_token,id,disname):
+    if intent == 'Customer - Description':
+        text_message = TextSendMessage(text='Customer Description')
+        line_bot_api.reply_message(reply_token,text_message)       
+
+def reqCompanyName(intent,text,reply_token,id,disname):
+    if intent == 'requestCompanyName':
+        text_message = TextSendMessage(text='Please enter company code')
+        line_bot_api.reply_message(reply_token,text_message)
+    
+def replySDN(intent,text,reply_token,id,disname):
+    if intent == 'ReplySDN':
+        access_Token_URL = 'https://login.microsoftonline.com/51cd216f-49b0-46d5-b6f2-dce309a29830/oauth2/v2.0/token'
+        configure_New_Token= {'grant_type' : 'client_credentials',
+                'scope' : 'https://api.businesscentral.dynamics.com/.default',
+                'client_id' : '2bb54e51-334d-4848-94cc-e44c9cc3f54a',
+                'client_secret' : 'Pg38Q~Ic5i2oJncaQs~SwRAPzmnjKSURqMynydjj'
+            }
+
+        response = requests.post(access_Token_URL, data=configure_New_Token)
+        jsonResponse = json.loads(response.text)
+        access_Token = jsonResponse['access_token']
+
+        api_URL = "https://api.businesscentral.dynamics.com/v2.0/51cd216f-49b0-46d5-b6f2-dce309a29830/SDNDEV2/api/AMCO/Item/v2.0/companies"
+
+        headers = {'Authorization' : 'Bearer '+access_Token}
+
+        resp = requests.get(api_URL , headers=headers)
+        json_result = resp.json()
+        for data in json_result['value']:
+            if (data['name']) == text:
+                Display = (data['displayName'])
+        text_message = TextSendMessage(text="รายชื่อบริษัท : {} ".format(Display))
+        line_bot_api.reply_message(reply_token,text_message)
+
+def replyKIN(intent,text,reply_token,id,disname):
+    if intent == 'ReplyKIN':
+        access_Token_URL = 'https://login.microsoftonline.com/51cd216f-49b0-46d5-b6f2-dce309a29830/oauth2/v2.0/token'
+        configure_New_Token= {'grant_type' : 'client_credentials',
+                'scope' : 'https://api.businesscentral.dynamics.com/.default',
+                'client_id' : '2bb54e51-334d-4848-94cc-e44c9cc3f54a',
+                'client_secret' : 'Pg38Q~Ic5i2oJncaQs~SwRAPzmnjKSURqMynydjj'
+            }
+
+        response = requests.post(access_Token_URL, data=configure_New_Token)
+        jsonResponse = json.loads(response.text)
+        access_Token = jsonResponse['access_token']
+
+        api_URL = "https://api.businesscentral.dynamics.com/v2.0/51cd216f-49b0-46d5-b6f2-dce309a29830/SDNDEV2/api/AMCO/Item/v2.0/companies"
+
+        headers = {'Authorization' : 'Bearer '+access_Token}
+
+        resp = requests.get(api_URL , headers=headers)
+        json_result = resp.json()
+        for data in json_result['value']:
+            if (data['name']) == text:
+                Display = (data['displayName'])
+        text_message = TextSendMessage(text="รายชื่อบริษัท : {} ".format(Display))
+        line_bot_api.reply_message(reply_token,text_message)
+    
 if __name__ == '__main__': app.run(debug=True)
