358777a59ac9af1ffadf09e71f53a27e7bbf5e73
MrBeam <Beamsangmin@gmail.com>
2022-08-04 16:05:48 +0700

    xxxx

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/test", methods=['POST'])
+def callback():
+    print('xxxxx')
+    return 'OK'
 
 if __name__ == '__main__': app.run(debug=True)
